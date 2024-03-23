57b6ace3b5de39473995e843ff85459eff3b5b22
henriqueAvner <avnerhdpb@gmail.com>
2024-03-23 20:10:15 -0300

    Feat: Estilizando os botões da página inicial e da página de login, além da criação de um formulário para login

--- roombooking/src/app/login/page.tsx
+++ roombooking/src/app/login/page.tsx
@@ -1,22 +1,29 @@
-import styles from './login.module.css';
-import Image from "next/image";
-import Link from "next/link";
-
-
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './login.module.css'; 
 export default function Login() {
     return (
         <>
         <main className={styles.main}>
-          <h1 className={styles.tittle}>Room Booking</h1>
-          <form>
-          <div className={styles.buttonsContainer}>
-              <Link href={"/login"}>
+          <h1 className={styles.title}>Room Booking</h1>
+          <form className={styles.form}>
+            <div className={styles.inputContainer}>
+                <input type="text" name="email" id="email" className={styles.input} placeholder="Email" />
+            </div>
+            <div className={styles.inputContainer}>
+                <input type="password" name="password" id="password" className={styles.input} placeholder="Password" />
+            </div>
+            <div className={styles.buttonsContainer}>
+              <Link href={"/home"}>
                 <button className={styles.button}>Entrar</button>
               </Link>
-              </div>
+              <Link href={"/"}>
+                <button className={styles.button}>Voltar</button>
+              </Link>
+            </div>
           </form>
           <div className={styles.hotel}>
-          <Image src="/images/initial-hotel.jpg" width={1520} height={760} alt={"Imagem de uma piscina de um hotel"} />
+            <Image src="/images/initial-hotel.jpg" width={1520} height={760} alt={"Imagem de uma piscina de um hotel"} />
           </div>
         </main>
         </>
